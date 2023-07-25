afb54bd3b6914902b5986f8dcb072a5e48341b96
Vishwanath Rao <vishymails@gmail.com>
2023-07-25 11:05:30 +0530

    Add files via upload

---
+++ Spy_Demo.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 25 10:03:27 2023
+
+@author: dr_vi
+"""
+
+tup1 = ("Java", "javascript", "pyspark")
+tup2 = ("Kotlin", "golang", "Erlang")
+
+final_tup = tup1 + tup2
+print(final_tup)
